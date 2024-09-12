bca87d6eeebe582c821e86ba58e11d2d4ec94f2a
Ukiah Sperry <ukiahsperry@gmail.com>
2024-09-11 22:33:12 -0400

    Adding Project to GitHub

---
+++ react-crash/src/App.jsx
@@ -0,0 +1,63 @@
+import  { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import React from 'react';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+import JobPage, {jobLoader} from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
+
+const App = () => {
+  // addJob function to add a new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return
+  };
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE'
+    });
+    return
+  };
+
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return
+  }
+
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element = {<MainLayout/>}>
+        <Route index element={<HomePage/>}/>
+        <Route path='/jobs' element={<JobsPage/>}/>
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}/>
+        {/* : signifies this is dynamitc, for differnt job pages */}
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader} />
+        <Route path='/jobs/:id' element={<JobPage deleteJob={ deleteJob }/>} loader={jobLoader} />
+        <Route path='*' element={<NotFoundPage/>}/>
+      </Route>
+      
+    )
+  );
+
+  return <RouterProvider router={router}/>
+};
+
+export default App
+
